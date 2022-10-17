4b52908231cf96c7145c5032d7f3091de748cea8
Matheus-FSantos <matheus.fs.contato@gmail.com>
2022-10-17 14:49:09 -0300

    API REST

---
+++ ADO 03 (API REST)/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+const usuarios = [];
+const senhas = [];
+
+server.get('/usuarios/logins', (requisicao, resposta) => {
+    return resposta.json(usuarios);
+});
+
+server.get('/usuarios/senhas', (requisicao, resposta) => {
+    return resposta.json(senhas);
+});
+
+server.post('/usuarios', (requisicao, resposta) => {
+    let { novoUsuario } = requisicao.body;
+    let { novaSenha } = requisicao.body;
+
+    usuarios.push(novoUsuario);
+    senhas.push(novaSenha);
+
+    return resposta.json(usuarios);
+});
+
+server.get('/usuarios', (requisicao, resposta) => {
+    const { verificaUsuario } = requisicao.body;
+    const { verificaSenha } = requisicao.body;
+    let usuario = false;
+    let senha = false;
+
+    usuarios.forEach((texto) => {
+        if(texto == verificaUsuario){
+            usuario = true;
+        }
+    });
+
+    senhas.forEach((texto) => {
+        if(texto == verificaSenha){
+            senha = true;
+        }
+    });
+
+    if(senha == true && usuario == true){
+        resposta.json('Usuario liberado');
+    } else if(senha == false && usuario == false){
+        resposta.json('Não registrado');
+    } else if (usuario == false){
+        resposta.json('Usuario incorreto');
+    } else {
+        resposta.json('Senha incorreta');
+    }
+});
+
+server.delete('/usuarios/:index', (requisicao, resposta) => {
+    const { index } = requisicao.params;
+
+    usuarios.splice((index - 1),1);
+    senhas.splice((index - 1),1);
+
+    return resposta.json('Usuário ' + index + ' deletado com sucesso');
+});
+
+server.listen(3000);
